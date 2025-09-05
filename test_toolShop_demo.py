851376103a33a96fb34afd1356209a4f86b8a87e
Vorleak Yek <vorleakyek@gmail.com>
2025-09-04 21:51:15 -0700

    Update to select the item after searching and make assertion

--- test_toolShop_demo.py
+++ test_toolShop_demo.py
@@ -22,20 +22,23 @@ async def test_login(page, test_data):
     await page.locator('[data-test="search-submit"]').click() 
 
     # Select the item with name "Pliers" 
-    search_result_links = page.locator('div[data-test="search_completed"] a')
+    await page.wait_for_load_state("networkidle")
+    search_result_links = page.locator('div[data-test="search_completed"] a')    
     count = await search_result_links.count()
 
     for i in range(count):
         product_link = search_result_links.nth(i)
-        product_name = await product_link.locator('div[data-test="product-name"]').inner_text()
+        product_name = await product_link.locator('[data-test="product-name"]').inner_text()
+        print(product_name)
         
         if product_name.lower().strip() == test_data['product-name-selected'].strip().lower():
             await product_link.click()
             break  # Optional: stop after clicking the first match
     
-    product_name_header = await page.locator('h1[data-test="product-name"]').inner_text()
+    product_name_header = await page.locator('[data-test="product-name"]').inner_text()
+    product_name_header_lower_case = product_name_header.lower().strip()
 
-    await expect(product_name_header.lower()).to_have_text(test_data['product-name-selected']) 
+    assert product_name_header_lower_case == test_data['product-name-selected'] 
 
 
     
